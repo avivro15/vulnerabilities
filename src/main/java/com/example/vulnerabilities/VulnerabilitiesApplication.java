077eea1e1655e34356557321c325befa1f3feff5
aviv <avivro1503@gmail.com>
2024-12-26 16:34:45 +0200

    Initial commit

---
+++ src/main/java/com/example/vulnerabilities/VulnerabilitiesApplication.java
@@ -0,0 +1,13 @@
+package com.example.vulnerabilities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VulnerabilitiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VulnerabilitiesApplication.class, args);
+	}
+
+}
